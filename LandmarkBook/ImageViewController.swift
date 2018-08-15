daf27953f0217fcc32bdf962a6e2d7c41962fa50
Ali Kumru <kumru4293@hotmail.com>
2018-08-15 16:40:06 +0300

    ilk commit

---
+++ LandmarkBook/ImageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ImageViewController.swift
+//  LandmarkBook
+//
+//  Created by Ali Kumru on 15.08.2018.
+//  Copyright © 2018 Ali Kumru. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
