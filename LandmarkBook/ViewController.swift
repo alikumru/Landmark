a5e35307bbc45fddd7231e9b37305dbc4e4eedea
Ali Kumru <kumru4293@hotmail.com>
2018-08-15 15:20:22 +0300

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Ali Kumru on 15.08.2018.
+//  Copyright © 2018 Ali Kumru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
